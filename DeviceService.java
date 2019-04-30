e911d942c04aab973d500c40c7872a309eefca3d
scotthancock <49205583+scotthancock@users.noreply.github.com>
2019-04-30 13:22:28 -0700

    Add files via upload

---
+++ DeviceService.java
@@ -0,0 +1,56 @@
+import java.io.ObjectOutputStream;
+import java.io.FileOutputStream;
+
+
+public class DeviceService {
+
+  public static void main(String[] args) {
+    User user = new User("UserNameScott", "scotthancock30@gmail.com", "Password");
+    Location loc = new Location(-122.07871769999998f, 37.3933521f);
+    Device device = new Device("ScottsDeviceID", "ScottsDeviceName", user, loc);
+
+    writeDeviceToDisk(device);
+  }
+
+  public void registerDevice(Device device){
+    // Do some validations if necessary
+    writeDeviceToDisk(device);
+  }
+
+  private static void writeDeviceToDisk(Device device) {
+
+		try {
+      ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(device.getDeviceId()));
+			oos.writeObject(device);
+			System.out.println("Done");
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+  private static Device retrieveDeviceFromDisk(String deviceId) {
+
+    try{
+
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+
+    return null;
+  }
+
+  private static void reportDeviceLost(String deviceId) {
+    try {
+    Device device = retrieveDeviceFromDisk(deviceId);
+    if(device != null){
+      // Do validation
+      device.setIsLost(true);
+    }
+
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+  }
+
+}
